96bbb61cef9eda07caabec4edbf8a1f93f70cd53
Wishwa8764 <wishwaaloka89764@gmail.com>
2024-08-08 19:23:10 +0530

    delete employee with sweet aleart

--- src/app/page/view-all-employee/view-all-employee.component.ts
+++ src/app/page/view-all-employee/view-all-employee.component.ts
@@ -2,28 +2,72 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-view-all-employee',
   standalone: true,
-  imports: [HttpClientModule,FormsModule,CommonModule],
+  imports: [HttpClientModule, FormsModule, CommonModule],
   templateUrl: './view-all-employee.component.html',
   styleUrl: './view-all-employee.component.css'
 })
 export class ViewAllEmployeeComponent {
 
-  public employeeList:any;
+  public employeeList: any;
 
-  constructor(private http:HttpClient){
+  constructor(private http: HttpClient) {
     this.loadEmployeeTable();
   }
 
-  loadEmployeeTable(){
+  loadEmployeeTable() {
     this.http.get("http://localhost:8080/emp-controller/get-all").subscribe(
-      res=>{
-        this.employeeList=res;
+      res => {
+        this.employeeList = res;
         console.log(res);
       }
     )
+  }
+
+  deleteEmployee(emplyee: any) {
+
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger"
+      },
+      buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+      title: "Are you sure?",
+      text: "You won't delete this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel!",
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.http.delete(`http://localhost:8080/emp-controller/delete-emp/${emplyee.id}`, { responseType: 'text' }).subscribe(res => {
+          this.loadEmployeeTable();
+          swalWithBootstrapButtons.fire({
+            title: "Deleted!",
+            text: "Your file has been deleted.",
+            icon: "success"
+          });
+          console.log(res);
+        })
+        
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        swalWithBootstrapButtons.fire({
+          title: "Cancelled",
+          text: "Your imaginary file is safe :)",
+          icon: "error"
+        });
+      }
+    });
+    
   }
 }
